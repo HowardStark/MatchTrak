b04ef8c616c0100e81d50ea36475eb86b23d44e2
Mickey <howard@getcoffee.io>
2015-12-16 12:18:38 -0800

    Starting to follow Sinatra best practices

--- server.rb
+++ server.rb
@@ -1,55 +1,48 @@
 #!/usr/bin/ruby
 
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra-websocket'
 require 'json'
 require 'thin'
 require 'tilt/erubis'
+require 'omniauth'
 
-set :server, 'thin'
-set :sockets, []
-set :port, 3000
-set :bind, '0.0.0.0'
+class MatchTrak < Sinatra::Base
+        set :sessions, true
+        set :server, 'thin'
+        set :sockets, []
+        set :port, 3000
+        set :bind, '0.0.0.0'
 
-@@data = '{"content": "Error: no data has been passed by CS:GO"}'
-
-post '/' do
-    payload = JSON.parse(request.body.read.to_s)
-    puts "####################"
-    puts JSON.pretty_generate(payload)
-    puts "####################"
-    @@data = payload
-    EM.next_tick { settings.sockets.each{|s| s.send(JSON.pretty_generate(payload)) } }
-    status 200
-end
-
-get '/api/player' do
-    content_type :json
-    JSON.pretty_generate(@@data)
-end
-
-get '/' do
-    @weapon = "Weapon Unavailable"
-    @ammos = Hash.new("Ammo Unavailable")
-    @health = "Health Unavailable"
-    if (!request.websocket?)
-        erb :index
-    else
-        request.websocket do |ws|
-          ws.onopen do
-            settings.sockets << ws
-          end
-          ws.onmessage do |msg|
-            EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
-          end
-          ws.onclose do
-            warn("websocket closed")
-            settings.sockets.delete(ws)
-          end
+        post '/' do
+            payload = JSON.parse(request.body.read.to_s)
+            EM.next_tick { settings.sockets.each{ |s| s.send(JSON.pretty_generate(payload)) } }
+            status 200
         end
-    end
-end
 
-get '/js' do
-    send_file 'script.js', :type => :js
+        get '/' do
+            if (!request.websocket?)
+                erb :index
+            else
+                request.websocket do |ws|
+                  ws.onopen do
+                    settings.sockets << ws
+                  end
+                  ws.onmessage do |msg|
+                    EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+                  end
+                  ws.onclose do
+                    warn("websocket closed")
+                    settings.sockets.delete(ws)
+                  end
+                end
+            end
+        end
+
+        get '/js' do
+            send_file 'script.js', :type => :js
+        end
+
+        # THIS ALLOWS THE SERVER TO BE STARTED DIRECTLY THROUGH RUNNING. DO NOT REMOVE #
+        run! if app_file == $0
 end
